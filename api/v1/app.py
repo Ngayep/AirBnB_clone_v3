94b198bb044594f2a24b6ce76e81e051ee191a66
Ngayep <courtesyofjess@gmail.com>
2024-08-05 09:21:55 +0100

    app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,7 +3,7 @@
 Flask Application instance
 """
 
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 import os
@@ -16,6 +16,12 @@ app.register_blueprint(app_views)
 def teardown_appcontext(exception):
     """Closes the storage on teardown"""
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found_error(error):
+    """Handle 404 errors and return a JSON response"""
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == '__main__':

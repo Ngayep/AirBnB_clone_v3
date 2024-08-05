94aebf5d8004b7ddbac135358d259eb2029c252b
Ngayep <courtesyofjess@gmail.com>
2024-08-05 09:10:11 +0100

    index modified

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -79,6 +79,9 @@ class FileStorage:
     def count(self, cls=None):
         """Count the number of objects in storage"""
         if cls:
-            return len([obj for obj in self.__objects.values()
-                        if isinstance(obj, cls)])
+            if isinstance(cls, type):
+                return len([obj for obj in self.__objects.values()
+                            if isinstance(obj, cls)])
+            else:
+                return 0
         return len(self.__objects)

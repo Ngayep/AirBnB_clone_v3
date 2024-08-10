998aa0592683cec250afcbe8308716fd21201e3f
Ngayep <courtesyofjess@gmail.com>
2024-08-10 08:46:12 +0100

    app.py modified

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,9 +7,13 @@ from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 import os
+from flask_cors import CORS
+
 
 app = Flask(__name__)
+CORS(app, resources={r'/*': {'origins': '0.0.0.0'}})
 app.register_blueprint(app_views)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
 
 @app.teardown_appcontext
@@ -24,7 +28,15 @@ def not_found_error(error):
     return jsonify({"error": "Not found"}), 404
 
 
-if __name__ == '__main__':
-    host = os.getenv("HBNB_API_HOST", "0.0.0.0")
-    port = int(os.getenv("HBNB_API_PORT", 5000))
-    app.run(host=host, port=port, threaded=True)
+@app.errorhandler(400)
+def bad_request(error):
+    """Handles 400 error"""
+    return jsonify({"error": error.description}), 400
+
+
+if __name__ == "__main__":
+    app.run(
+        host=getenv('HBNB_API_HOST', '0.0.0.0'),
+        port=int(getenv('HBNB_API_PORT', '5000')),
+        threaded=True
+    )
